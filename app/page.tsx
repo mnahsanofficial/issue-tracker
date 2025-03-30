198d748b887a817b920df00d2181249e9044f0ff
mnahsanofficial <mnahsanofficial@gmail.com>
2025-03-31 03:57:41 +0600

    Day-02: component setup

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main><h1>Hello World</h1>
-    <Link href='/users'>Users</Link></main>
+    <Link href='/users'>Users</Link>
+    </main>
+    
   )
 }
