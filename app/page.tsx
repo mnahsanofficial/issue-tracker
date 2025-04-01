2382daf8c52d45952857ff8e3734a534b3ac5dec
mnahsanofficial <mnahsanofficial@gmail.com>
2025-04-01 23:04:00 +0600

    Day-03: developemnt setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,48 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
+import { Metadata } from "next";
+import prisma from "./prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: {
+      status: 'OPEN'
+    }
+  })
+
+  const inProgress = await prisma.issue.count({
+    where: {
+      status: 'IN_PROGRESS'
+    }
+  })
+
+  const closed = await prisma.issue.count({
+    where: {
+      status: 'CLOSED'
+    }
+  })
+
+  const issueCounts = {
+    open,
+    inProgress,
+    closed
+  }
+
+
   return (
-    <main><h1>Hello World</h1>
-    <Link href='/users'>Users</Link>
-    </main>
-    
+    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+      <Flex direction='column' gap='5'>
+        <IssueSummary issueCounts={issueCounts} />
+        <IssueChart issueCounts={issueCounts} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
   )
+}
+
+export const metadata: Metadata = {
+  title: 'Issue Tracker - Dashboard',
+  description: 'View a summary of project issues'
 }
